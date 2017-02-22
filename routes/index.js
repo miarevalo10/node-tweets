548cd0c96e39cdc0551d9cb4ed33af769f23b223
María Arévalo <mi.arevalo10@uniandes.edu.co>
2017-02-22 18:01:59 -0500

    First commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+var express = require('express');
+var router = express.Router();
+var json = require('../tweets.json')
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // console.log(json[1].text);
+  res.render('index',{objects:json, title:'Tweets'});
+
+});
+
+module.exports = router;
